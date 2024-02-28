11a77143bb9bfcd408129cc978fd26ce3384a917
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-02-28 11:21:07 +0400

    Create ISaveLoadService.cs

---
+++ ISaveLoadService.cs
@@ -0,0 +1,22 @@
+// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+namespace AbyssMoth.Internal.Codebase.Infrastructure.StorageService
+{
+    public interface ISaveLoadService
+    {
+        public void Save<TData>(TData data, string key);
+        public TData Load<TData>(string key, TData defaultValue = default);
+#if UNITY_EDITOR
+        public void SetCustomPathInEditor(string customPath);
+#endif
+    }
+}
